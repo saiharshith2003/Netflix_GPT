c6be8f040d7ef2faa0573c19d1d5697a3f05a3dd
saiharshith2003 <harshithsait2003@gmail.com>
2024-02-11 14:33:31 +0530

    commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { background } from "../utils/logos";
 import Header from "./Header";
+import { checkValidation } from "../utils/validate";
 
 const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
 
+    const email = useRef(null);
+    const password = useRef(null);
+    const handleValidation = () => {
+        const message = checkValidation(email.current.value, password.current.value)
+        setErrorMessage(message)
+    }
     const toggleForm = () => {
         setIsSignInForm(!isSignInForm);
     }
@@ -15,13 +23,14 @@ const Login = () => {
             <div className="absolute inset-0">
                 <img className="w-full h-full object-cover" src={background} alt="Background" />
             </div>
-            <form className="absolute w-3/12 text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black p-8 rounded-xl shadow-lg bg-opacity-85" style={{ top: '55%' }}>
+            <form onSubmit={(e) => { e.preventDefault() }} className="absolute w-3/12 text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black p-8 rounded-xl shadow-lg bg-opacity-85" style={{ top: '55%' }}>
                 <h1 className="text-3xl font-bold py-3  m-2">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
                 {!isSignInForm && <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="text" placeholder="Enter full name" />}
-                <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="text" placeholder="Email or phone number" />
-                <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="password" placeholder="Enter password" />
+                <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="text" ref={email} placeholder="Enter Email" />
+                <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="password" ref={password} placeholder="Enter password" />
                 {!isSignInForm && <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="password" placeholder="Re-enter password" />}
-                <button className="py-3 px-4 m-2 bg-red-500 rounded-lg w-full">Sign in</button>
+                <p className="py-1 mx-3 my-1 font-bold text-red-600">{errorMessage}</p>
+                <button className="py-3 px-4 m-2 bg-red-600 rounded-lg w-full" onClick={handleValidation}>Sign in</button>
                 <p className="py-2 m-2">{isSignInForm ? "New to Netflix? " : "Already a User? "}<span className="text-base font-semibold cursor-pointer" onClick={toggleForm}>{isSignInForm ? "Sign up now" : "Sign in now"}</span></p>
             </form>
         </div>
