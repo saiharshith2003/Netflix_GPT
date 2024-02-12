f53c89438245f7099f7d08d0afffa9da7c85e2e3
saiharshith2003 <harshithsait2003@gmail.com>
2024-02-12 11:53:20 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import { netflixLogo } from "../utils/logos";
-
+import { useSelector } from "react-redux";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+    const navigate = useNavigate();
+    const user = useSelector((store) => store.user)
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            navigate("/")
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
     return (
-        <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
+        <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
             <img className="w-44 mx-28" src={netflixLogo} alt="Logo" />
+            {user && (<div className="py-5 px-8">
+
+                <button className="px-4 py-2 bg-red-600 rounded-lg" onClick={handleSignOut}>Sign out</button>
+            </div>)}
         </div>
+
     );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,15 +2,19 @@ import { useRef, useState } from "react";
 import { background } from "../utils/logos";
 import Header from "./Header";
 import { checkValidation } from "../utils/validate";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
-
+    const name = useRef(null);
     const email = useRef(null);
     const password = useRef(null);
-
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     const handleValidation = () => {
         const message = checkValidation(email.current.value, password.current.value)
         setErrorMessage(message)
@@ -18,25 +22,40 @@ const Login = () => {
         if (!isSignInForm) {
             createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
                 .then((userCredential) => {
-                    // Signed up 
                     const user = userCredential.user;
-                    // ...
                     console.log(user)
+                    updateProfile(user, {
+                        displayName: name.current.value, photoURL: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dsmall%2Blogo&psig=AOvVaw1HatkYervzSzIdTDk8jpBm&ust=1707745708963000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOC8ltW2o4QDFQAAAAAdAAAAABAE"
+                    }).then(() => {
+                        const { uid, email, displayName, photoURL } = auth.currentUser
+                        dispatch(addUser({
+                            uid: uid,
+                            email: email,
+                            displayName: displayName,
+                            photoURL: photoURL,
+                        }))
+                    }).catch((error) => {
+
+                        setErrorMessage(error.message)
+                    });
+                    setIsSignInForm(!isSignInForm)
+                    navigate("/")
+                    alert("Account created succesfully")
                 })
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessag = error.message;
-                    // ..
+
                     setErrorMessage(errorCode + errorMessag);
                 });
         }
         else {
             signInWithEmailAndPassword(auth, email.current.value, password.current.value)
                 .then((userCredential) => {
-                    // Signed in 
+
                     const user = userCredential.user;
-                    // ...
                     console.log(user)
+                    navigate("/browse")
                 })
                 .catch((error) => {
                     const errorCode = error.code;
@@ -57,7 +76,7 @@ const Login = () => {
             </div>
             <form onSubmit={(e) => { e.preventDefault() }} className="absolute w-3/12 text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black p-8 rounded-xl shadow-lg bg-opacity-85" style={{ top: '55%' }}>
                 <h1 className="text-3xl font-bold py-3  m-2">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-                {!isSignInForm && <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="text" placeholder="Enter full name" />}
+                {!isSignInForm && <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="text" ref={name} placeholder="Enter full name" />}
                 <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="text" ref={email} placeholder="Enter Email" />
                 <input className="w-full px-4 py-4 m-2 border bg-gray-950 rounded-lg bg-opacity-35" type="password" ref={password} placeholder="Enter password" />
 
